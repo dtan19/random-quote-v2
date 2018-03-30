46eb60612dce59ad4af275bdefc8f6f61aa90b54
filmsterdavid <tanner.davide@gmail.com>
2018-03-29 20:01:40 -0600

    Finished Random Quote Generator App.

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,8 @@
-var quotes = [
-    {quote: 'Test quote', source: 'name', citation: 'Show', year: '2000'},
-    {quote: 'Test quote1', source: 'name1', citation: 'Show', year: '2000'},
-    {quote: 'Test quote2', source: 'name2', citation: 'Show', year: '2000'},
-    {quote: 'Test quote3', source: 'name3', citation: 'Show', year: '2000'},
-    {quote: 'Test quote4', source: 'name4', citation: 'Show', year: '2000'}
+// an array of quotes
+let quotes = [
+    {quote: 'To see the world, things dangerous to come to, to see behind walls, to draw closer, to find each other and to feel. That is the purpose of life.', source: 'Walter Mitty', citation: 'The Secret Life of Walter Mitty', year: '2013'},
+    {quote: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.', source: 'Helen Keller'},
+    {quote: 'Life is short, live it. Love is rare, grab it. Anger is bad, dump it. Fear is awful, face it. Memories are sweet, cherish it.'},
+    {quote: 'Time and silence are the most luxurious things today.', source: 'Tom Ford'},
+    {quote: 'Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it.', source: 'Ferris Bueller', citation: "Ferris Bueller's Day Off", year: '1986'}
 ];
--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
+
+
+//function that returns and array index and the quote from that index.
+    function getRandomQuote() {
+        let index = Math.floor(Math.random() * quotes.length );
+        return quotes[index];
+    }
+
+//function that prints the quote provided by the random quote generator function.
+    function printQuote() {
+        let quoteBlock = getRandomQuote();
+        let html = '';
+        html += '<p class="quote">' + quoteBlock.quote + '</p>';
+
+        // if source is defined it will be displayed, if it's undefined the source will display: 'unknown'
+        if (quoteBlock.source !== undefined) {
+            html += '<p class="source">' + quoteBlock.source;
+        } else if (quoteBlock.source === undefined) {
+            html += '<p class="source">' + 'Unknown';
+        }
+
+        // if citation is defined it will be displayed.
+        if (quoteBlock.citation !== undefined) {
+            html += '<span class="citation">' + quoteBlock.citation + '</span>';
+        }
+
+        // if the quote has a year it will be displayed.
+        if (quoteBlock.year !== undefined) {
+            html += '<span class="year">' + quoteBlock.year + '</span>';
+        }
+
+        html += '</p>';
+
+        // Loads HTML string to web page.
+        document.getElementById('quote-box').innerHTML = html;
+    }
+
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
-
-function getRandomQuote (quotes){
-    var newQuote = Math.floor( Math.random() * 6 ) + 1;
-    return newQuote;
-}
-
-function print(message) {
-    var div = document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-}
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("loadQuote").addEventListener("click", function() { printQuote();
+        }, false);
+    });
 
 
-var printQuote = getRandomQuote();
+
+
+
 
 
 
